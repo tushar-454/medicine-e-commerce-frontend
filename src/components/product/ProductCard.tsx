79381ffa27a6c1098843de07e094b97ad6ccf358
tushar-454 <imtushar454@gmail.com>
2024-08-12 22:40:13 +0600

    UI: product card add product varient and view details button

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,9 +1,11 @@
 import { ProductCardProps } from '@/types/product';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { TbCurrencyTaka } from 'react-icons/tb';
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [isVarientOpen, setIsVarientOpen] = useState(false);
+  const [selectedVariant, setSelectedVariant] = useState(0);
   return (
     <div className='rounded-lg bg-white shadow-lg'>
       <div className='rounded-t-lg'>
@@ -15,7 +17,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           className='h-60 w-full rounded-t-lg object-cover'
         />
       </div>
-      <div className='space-y-2 p-3'>
+      <div className='relative space-y-2 p-3'>
         <p className='text-2xl font-semibold'>{product.name}</p>
         <p className='text-xl font-semibold text-surfie-green-400'>
           {product.description}
@@ -39,8 +41,44 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
             {product.discount}% OFF
           </span>
         </p>
-
-        <button className='atc-button block w-full'>Add to cart</button>
+        <div
+          className={`absolute left-0 top-0 flex h-full w-full flex-col rounded-b-lg bg-black/80 backdrop-blur transition-all ${isVarientOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}
+        >
+          <p className='border-b-2 border-black bg-white px-2 pb-2 text-2xl font-semibold'>
+            {product.name}
+          </p>
+          <div className='flex-grow'>
+            {product?.variants?.map((variant, idx) => (
+              <div
+                key={variant._id}
+                onClick={() => setSelectedVariant(idx)}
+                className={`flex cursor-pointer gap-5 border-b-2 p-2 text-lg active:bg-surfie-green-200 ${selectedVariant === idx ? 'bg-surfie-green-200' : 'bg-white text-black'}`}
+              >
+                <span>{variant.dose}</span>
+                <span>{variant.package_size}</span>
+                <span>{variant.price}</span>
+              </div>
+            ))}
+          </div>
+          <button
+            onClick={() => setIsVarientOpen(!isVarientOpen)}
+            className='mt-4 block w-full bg-neutral-200 px-3 py-2 text-black'
+          >
+            View Details
+          </button>
+          <button
+            onClick={() => setIsVarientOpen(!isVarientOpen)}
+            className='atc-button mt-2 block w-full rounded-b-lg rounded-t-none'
+          >
+            Add to cart
+          </button>
+        </div>
+        <button
+          onClick={() => setIsVarientOpen(true)}
+          className='atc-button block w-full'
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
