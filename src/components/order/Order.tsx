714d3f5749c0bcfec79d4ef945cdf6316796741a
tushar-454 <imtushar454@gmail.com>
2024-08-14 14:22:22 +0600

    feat: implement order creation

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -1,18 +1,76 @@
-import { toggleOrder } from '@/feature/publicStateSlice/publicStateSlice';
+import { getCarts } from '@/api/cart';
+import { fetchProduct } from '@/api/product';
+import { userLogin } from '@/api/user';
+import {
+  toggleModal,
+  toggleOrder,
+} from '@/feature/publicStateSlice/publicStateSlice';
 import { AppDispatch, RootState } from '@/store/store';
 import { CartType } from '@/types/cart';
 import { OrderProps } from '@/types/order';
+import axios from '@/utils/axios';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import Modal from '../shared/Modal';
 
 const Order: React.FC<OrderProps> = ({ openOrder }) => {
   const dispatch = useDispatch<AppDispatch>();
   const { carts } = useSelector((state: RootState) => state.cart);
   const { user } = useSelector((state: RootState) => state.user);
+  const [shippingInfo, setShippingInfo] = React.useState({
+    division: user?.division,
+    district: user?.district,
+    upazila: user?.upazila,
+    address: user?.address,
+    phone: '',
+    password: '',
+  });
+
+  // handle change function
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setShippingInfo({
+      ...shippingInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // handle update profile shipping info
+  const handleUpdateProfile = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { division, district, upazila, address, phone, password } =
+      shippingInfo;
+    try {
+      if (
+        !division ||
+        !district ||
+        !upazila ||
+        !address ||
+        !phone ||
+        !password
+      ) {
+        toast.error('All fields are required');
+        return;
+      }
+      const res = await axios.put(`/user/${user?.email}`, {
+        division,
+        district,
+        upazila,
+        address,
+        phone,
+        password,
+      });
+      if (res.status === 200) {
+        toast.success('Profile updated successfully');
+        if (user) dispatch(userLogin({ email: user?.email, password }));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // handle order now function
-  const handleOrderNow = () => {
+  const handleOrderNow = async () => {
     const order = {
       user: user?._id,
       products: carts.map((cart: CartType) => {
@@ -21,7 +79,7 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
         return {
           product: cart.product._id,
           quantity: cart.quantity,
-          varient: cart.varient,
+          varient: cart.product.variants[cart.varient],
           total: withDiscount,
         };
       }),
@@ -30,16 +88,36 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
         const withDiscount = total - (total * cur.product.discount) / 100;
         return acc + withDiscount;
       }, 0),
-      paymentMethod: 'card',
+      paymentMethod: 'cash',
       shippingInfo: {
         division: user?.division,
         district: user?.district,
         upazila: user?.upazila,
         address: user?.address,
       },
-      Phone: '+8801234567890',
+      phone: shippingInfo.phone,
     };
-    console.log(order);
+    if (!shippingInfo.phone) {
+      toast.error('Phone number is required');
+      return;
+    }
+    try {
+      const res = await axios.post('/order/create', order);
+      if (res.status === 201) {
+        toast.success('Order placed successfully');
+        dispatch(toggleOrder());
+        dispatch(toggleModal());
+        carts.forEach(async (cart: CartType) => {
+          await axios.delete(`/cart/${cart._id}`);
+          if (user) {
+            dispatch(getCarts(user?._id));
+            dispatch(fetchProduct({ category: '', user: user?._id }));
+          }
+        });
+      }
+    } catch (error) {
+      toast.error('Order failed! Check all fields are filled');
+    }
   };
 
   // modal open close function
@@ -52,7 +130,7 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
         <div className='w-full lg:w-1/2'>
           <h2 className='text-lg font-medium'>Shipping Address</h2>
           {/* shpping form  */}
-          <form className='mt-5'>
+          <form className='mt-5' onSubmit={handleUpdateProfile}>
             <div className='mb-4'>
               <label htmlFor='division' className='label'>
                 Division
@@ -60,8 +138,11 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
               <input
                 type='text'
                 id='division'
+                name='division'
                 className='input'
                 placeholder='Enter your division'
+                value={shippingInfo.division}
+                onChange={handleChange}
               />
             </div>
             <div className='mb-4'>
@@ -74,6 +155,8 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
                 name='district'
                 className='input'
                 placeholder='Enter your district'
+                value={shippingInfo.district}
+                onChange={handleChange}
               />
             </div>
             <div className='mb-4'>
@@ -86,6 +169,8 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
                 name='upazila'
                 className='input'
                 placeholder='Enter your upazila'
+                value={shippingInfo.upazila}
+                onChange={handleChange}
               />
             </div>
             <div className='mb-4'>
@@ -98,6 +183,8 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
                 name='address'
                 className='input'
                 placeholder='Enter your address'
+                value={shippingInfo.address}
+                onChange={handleChange}
               />
             </div>
             <div className='mb-4'>
@@ -110,6 +197,22 @@ const Order: React.FC<OrderProps> = ({ openOrder }) => {
                 name='phone'
                 className='input'
                 placeholder='Enter your phone'
+                value={shippingInfo.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='password' className='label'>
+                Password
+              </label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                className='input'
+                placeholder='Enter your password'
+                value={shippingInfo.password}
+                onChange={handleChange}
               />
             </div>
             <button className='atc-button'>Update Profile</button>
